1d110229d1a92973c2b54fa7b3f820498dae01ea
autumn <892049513@qq.com>
2021-05-31 20:40:05 +0800

    进度24%吧

--- miniprogram/pages/novel/novel.js
+++ miniprogram/pages/novel/novel.js
@@ -1,54 +1,26 @@
-// pages/detail/detail.js
-var time = require('../../utils/utils.js');
-var that = ''
-
+// pages/novel/novel.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   content:[],
-   comment:[],
-   times:[],
+  novel:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    that=this
+     var that=this
     wx.request({
-      url: 'http://m.taoyuewenhua.com/ajax/book?sourceName='+options.sourceName+
+      url: 'http://m.taoyuewenhua.com/ajax/chapter_content?sourceName='+options.sourceName+
       '&sourceId='+options.sourceId,
       method:'GET',
       success(res){
         console.log(res.data.data);
         that.setData({
-          content:res.data.data
-        })
-      },
-      fail(err){
-        console.log(err);
-      }
-    }),
-    wx.request({
-      url: 'http://m.taoyuewenhua.com/ajax/top_comments?sourceName='+options.sourceName+
-      '&sourceId='+options.sourceId,
-      method:'GET',
-      success(res){
-        let times= []
-        console.log(res.data);
-        that.setData({
-          comment:res.data.data,
-        })
-        for(var i=0;i<that.data.comment.length;i++){
-          // times.push( time.formatTime(that.data.comment[i].createdOn, 'h:m'))
-          that.data.comment[i].times = time.formatTime(that.data.comment[i].createdOn, 'h:m')
-        }
-        console.log(that.data.comment);
-        that.setData({
-          comment:that.data.comment
+          novel:res.data.data
         })
       },
       fail(err){
@@ -56,7 +28,7 @@ Page({
       }
     })
   },
- 
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
